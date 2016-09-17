8e334f5d113c7b6049a85d95dfd19bd9621481e5
Jacob Siegel <jacobsiegel35@gmail.com>
2016-09-16 23:53:18 -0400

    Added a quick hack to disable stupid fucking terminal messages that xcode 8 produces

--- CornellHack/CornellHack/GraphViewController.swift
+++ CornellHack/CornellHack/GraphViewController.swift
@@ -13,6 +13,8 @@ class GraphViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		
+		print("Starting up th app")
         // Do any additional setup after loading the view.
     }
 }
