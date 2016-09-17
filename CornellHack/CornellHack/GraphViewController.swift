f0509093473f13c4d2a5ff0b56154363cba40902
Jacob Siegel <jacobsiegel35@gmail.com>
2016-09-16 23:45:49 -0400

    more quick setup changes

---
+++ CornellHack/CornellHack/GraphViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GraphViewController.swift
+//  CornellHack
+//
+//  Created by Jacob Siegel on 9/16/16.
+//  Copyright © 2016 Jacob Siegel. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
