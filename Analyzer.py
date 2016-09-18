7cc93f22881256d7319606cbb73ea6a5e0b2056b
kyleskom <kyleskom@buffalo.edu>
2016-09-17 22:13:27 -0400

    Replace ID
    NEW ID

--- Analyzer.py
+++ Analyzer.py
@@ -1,6 +1,8 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
 from json import JSONEncoder
 import csv
+import time
 
 headers = {
     # Request headers
@@ -13,24 +15,31 @@ params = urllib.parse.urlencode({
 
 exampleFile = open('SavedData.csv')
 exampleReader = csv.reader(exampleFile)
-x = 1
+#x = 1
+blah = []
 for row in exampleReader:
     jsonString = JSONEncoder().encode({
         "documents": [
          {
           "language": "en",
-          "id": str(x),
+          "id": "trump",
           "text": str(row)
          }]
     })
-    x += 1
+    #x += 1
 
     try:
-     conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-     conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, jsonString, headers)
-     response = conn.getresponse()
-     data = response.read()
-     print(data)
-     conn.close()
+        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, jsonString, headers)
+        response = conn.getresponse()
+        data = response.read().decode()
+        what_you_want = json.loads(data)["documents"][0]
+        what_you_want["time"] = str((round(time.time() * 1000)))
+        blah.append(what_you_want)
+        conn.close()
     except Exception as e:
-     print("[Errno {0}] {1}".format(e.errno, e.strerror))
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+blah_blah = {"data": blah}
+with open("Data.json", "w") as file:
+    file.write((json.dumps(blah_blah, sort_keys=True, indent=2, separators=(',', ': '))))
